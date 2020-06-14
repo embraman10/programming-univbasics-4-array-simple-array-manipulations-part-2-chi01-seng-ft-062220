228578bd9e335b45b00ccd975581a8b920a9d39d
Emily Braman <embraman10@gmail.com>
2020-06-14 00:00:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,11 @@
-def using_concat
+def using_concat(array)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   my_favorite_things.concat(["sports cars", "flatiron school"])
 end
 
 
-def using_inssert
+def using_insert
+  list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
 
 end
 
