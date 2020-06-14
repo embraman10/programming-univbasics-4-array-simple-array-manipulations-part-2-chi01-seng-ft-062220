0fc01a48e7d70a5bf521de95d375f88609c7ace8
Emily Braman <embraman10@gmail.com>
2020-06-14 00:21:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,8 +6,7 @@ def using_insert (array, element)
   array.insert(4, element)
 end
 
-def using_uniq
-  haircuts = ["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"]
+def using_uniq (array)
   array.uniq
 end
 
