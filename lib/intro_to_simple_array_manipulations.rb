3cdfea7d1037c1f4c055bebb3e6a388c840c45b6
Emily Braman <embraman10@gmail.com>
2020-06-14 00:13:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,5 @@
 def using_concat(arrays)
-  my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-  more_favs = ["sports cars", "flatiron school"]
-  array.concat(my_favorite_things,more_favs)
+  ["raindrops on roses", "whiskers on kittens"].concat(["sports cars", "flatiron school"])
 end
 
 
