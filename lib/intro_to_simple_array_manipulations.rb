4cf98308b30472bc1d09fdc1aa06dcae2ed9d4a5
Emily Braman <embraman10@gmail.com>
2020-06-14 00:18:46 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,10 +4,9 @@ def using_concat(arrays)
   all_my_favs = array.concat(my_favorite_things, more_favs)
 end
 
-
 def using_insert
   list_of_programming_languages = ["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"]
-
+  list_of_programming_languages.insert(4, "Python")
 end
 
 def using_uniq
